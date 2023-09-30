a6f8c88761b659c9a7d68fa218b7ba0c800b88b4
revisionismer <cherry2137@naver.com>
2023-09-30 17:48:07 +0900

    Update Code

---
+++ src/views/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../fragments/Footer";
+import Header from "../fragments/Header";
+
+const Index = () => {
+    return (
+    <>
+        <Header/>
+        <Footer/>
+    </>
+    );
+};
+
+export default Index;
